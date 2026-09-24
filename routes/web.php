1a71c53e5cb9a76384c97b6303e983cb5ed95f7a
JoseAM <jose.araiza0355@alumnos.udg.mx>
2026-09-24 16:42:11 -0600

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/formulario.horoscopo', function () {
+    return view('formulario');
+});
